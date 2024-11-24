5909739449a2bff2abb488ddba63b99374a95f99
Moaz-Esmail <moazmohamed.ba@gmail.com>
2024-11-24 16:30:28 +0200

    Fix: printing statements

--- src/packages/Gate.java
+++ src/packages/Gate.java
@@ -3,7 +3,10 @@ package src.packages;
 public class Gate {
     private final CustomSemaphore gateSemaphore;
     private int id;
-
+    
+    private synchronized void log(String message) {
+        System.out.println(message);
+    }
     public Gate(int id, CustomSemaphore sem) {
         this.id = id;
         this.gateSemaphore = sem;
@@ -12,33 +15,32 @@ public class Gate {
     public int enter(Car c) throws InterruptedException {
         int waitingTime = 0;
 
-        System.out.println(c.toString() + " arrived at time " + c.arrivalTime + ".");
+       log(c.toString() + " arrived at time " + c.arrivalTime + ".");
 
         while (!gateSemaphore.tryAcquire()) {
             if (waitingTime == 0) {
-                System.out.println(c.toString() + " waiting for a spot.");
+                log(c.toString() + " waiting for a spot.");
             }
             Thread.sleep(src.Main.TIME_UNIT); // Wait for a spot
             waitingTime++;
         }
 
         // Print parking status when the car parks
-        System.out.println(c.toString() + " parked"
+        log(c.toString() + " parked"
                 + (waitingTime > 0 ? " after waiting for " + waitingTime + " units of time" : ""));
-        updateParkingStatus();
         return waitingTime;
     }
 
-    public void leave(Car c) {
+    public synchronized void leave(Car c) {
+        log(c.toString() + " left after " + c.parkingDuration + " units of time.");
         gateSemaphore.release();
-
-        System.out.println(c.toString() + " left after " + c.parkingDuration + " units of time.");
         updateParkingStatus();
     }
 
+
     private synchronized void updateParkingStatus() {
         int occupiedSpots = src.Main.PARK_SPOTS_COUNT - gateSemaphore.availablePermits();
-        System.out.println("(Parking Status: " + occupiedSpots + " spots occupied)");
+        log("(Parking Status: " + occupiedSpots + " spots occupied)");
     }
 
     public String toString() {
